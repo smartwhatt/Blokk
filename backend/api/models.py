9c8a0e44b8a41bc48673cdbd6a6a448ae3bef860
Jimmy_Tempest <smartwattana@gmail.com>
2022-01-08 14:16:48 +0700

    feat: add public and private key to User

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,33 @@
+import rsa
+
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    publickey = models.TextField(max_length=5000, blank=True, null=True)
+    privatekey = models.TextField(max_length=5000, blank=True, null=True)
+
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'password']
+
+
+    def __str__(self):
+        return self.username
+
+    def generateKey(self):
+        (pubkey, privkey) = rsa.newkeys(2048)
+        self.publickey = pubkey.save_pkcs1()
+        self.privatekey = privkey.save_pkcs1()
+        self.save()
+        return (pubkey, privkey)
+    
+    def create_user(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.set_password(password)
+        self.generateKey()
+        self.save()
+        return self
