17b38947918f13a0bc88d40f0f596d04ada7684b
Jimmy_Tempest <smartwattana@gmail.com>
2022-01-08 14:20:33 +0700

    feat: add User serializer

---
+++ backend/api/serializers.py
@@ -0,0 +1,18 @@
+from .models import User
+from rest_framework.serializers import ModelSerializer
+
+class UserSerializers(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.generateKey()
+        user.save()
+        return user
