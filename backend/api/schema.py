40b54cd8ee2632b5a40b96f4803e14c46470cc23
Jimmy_Tempest <smartwattana@gmail.com>
2022-01-09 00:10:51 +0700

    feat: add graphql for wallets

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -29,6 +29,10 @@ class Query(graphene.ObjectType):
     user_by_username = graphene.Field(UserType, username=graphene.String())
 
     all_wallets = graphene.List(WalletType)
+    wallet_by_id = graphene.Field(WalletType, id=graphene.Int())
+    wallets_by_user = graphene.List(WalletType, user=graphene.String())
+    wallets_by_currency = graphene.List(WalletType, currency=graphene.String())
+
     all_transactions = graphene.List(TransactionType)
     all_currencies = graphene.List(CurrencyType)
 
@@ -46,5 +50,27 @@ class Query(graphene.ObjectType):
         if username is not None:
             return User.objects.get(username=username)
         return None
+    
+    def resolve_all_wallets(self, info, **kwargs):
+        return Wallet.objects.all()
+    
+    def resolve_wallet_by_id(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id is not None:
+            return Wallet.objects.get(pk=id)
+        return None
+    
+    def resolve_wallets_by_user(self, info, **kwargs):
+        user = kwargs.get('user')
+        if user is not None:
+            return Wallet.objects.filter(user=user)
+        return None
+    
+    def resolve_wallets_by_currency(self, info, **kwargs):
+        currency = kwargs.get('currency')
+        if currency is not None:
+            return Wallet.objects.filter(currency=currency)
+        return None
+
 
 schema = graphene.Schema(query=Query)
