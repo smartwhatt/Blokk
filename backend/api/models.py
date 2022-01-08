89445cfd9b53ba24e1d01a668f2a49836ff3465f
Jimmy_Tempest <smartwattana@gmail.com>
2022-01-08 14:25:17 +0700

    feat: add User profile picture to User

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,9 +5,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
+def pfp_path(instance, filename):
+    return f'pfp/{instance.username}/{filename}'
+
 class User(AbstractUser):
     publickey = models.TextField(max_length=5000, blank=True, null=True)
     privatekey = models.TextField(max_length=5000, blank=True, null=True)
+
+    pfp = models.ImageField(upload_to=pfp_path, blank=True, null=True)
 
 
     USERNAME_FIELD = 'username'
@@ -31,3 +37,19 @@ class User(AbstractUser):
         self.generateKey()
         self.save()
         return self
+    
+    def create_superuser(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.set_password(password)
+        self.is_superuser = True
+        self.is_staff = True
+        self.generateKey()
+        self.save()
+        return self
+    
+    def get_publickey(self):
+        return self.publickey
+    
+    def get_privatekey(self):
+        return self.privatekey
