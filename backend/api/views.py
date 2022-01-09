08179113bc977b1428185a6e8c28b270fe5911f3
Jimmy_Tempest <smartwattana@gmail.com>
2022-01-09 18:03:12 +0700

    fix: allow csrf on graphql

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework.response import Response# Create your views here.
+from rest_framework.response import Response
 
 from .models import *
+
+# Create your views here.
+
 
 @api_view(['GET'])
 def index(request):
     return Response({'message': 'Hello, world!'})
+
 
 @api_view(['POST'])
 def register(request):
     username = request.data['username']
     email = request.data['email']
     password = request.data['password']
-    user = User.objects.create_user(username=username, email=email, password=password)
+    user = User.objects.create_user(
+        username=username, email=email, password=password)
     user.save()
     return Response({'message': 'User created successfully'})
