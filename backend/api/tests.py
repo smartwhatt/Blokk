54abd67d123a00e8800aa34bd7dcb23b1fc48056
Jimmy_Tempest <smartwattana@gmail.com>
2022-01-14 21:34:08 +0700

    fix: correct test for creating curreny

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -291,6 +291,21 @@ class CurrencyAPITestCase(TestCase):
         self.assertEqual(response.data['symbol'], "ETH")
         self.assertEqual(response.data['admin'], self.user.id)
         self.assertEqual(response.data['initial_balance'], 100)
+    
+    def test_api_can_create_a_currency_with_initial_balance_and_market_cap(self):
+        """Test the api has currency creation capability."""
+        url = reverse('currency')
+        data = {
+            'name': "Ethereum",
+            'symbol': "ETH",
+            'initial_balance': 100,
+            'market_cap': 100
+        }
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + self.auth_token.data['access'])
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['message'], "Initial balance is required for currency without market cap")
 
     def test_api_try_create_a_currency_without_login(self):
         """Test the api has currency creation capability."""
